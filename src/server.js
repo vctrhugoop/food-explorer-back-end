25ed325a046dc44b61c9ed23a31c8ec179f821a9
vctrhugoop <vhdestroy@hotmail.com>
2024-04-09 07:10:15 -0300

    ✨ feat: add and config cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require('express-async-errors');
 
+const cors = require('cors');
+
 const AppError = require('./utils/AppError');
 
 const express = require('express');
@@ -7,6 +9,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
